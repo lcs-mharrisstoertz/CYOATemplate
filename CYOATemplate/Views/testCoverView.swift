1c5ba44d6cac0e9e593b6a4d44f3f30b422673ac
Griffin Muir <gmuir@lcs.on.ca>
2024-06-13 11:19:11 -0400

    made a seperate view to test the video on before adding it to the entry page

---
+++ CYOATemplate/Views/testCoverView.swift
@@ -0,0 +1,18 @@
+//
+//  testCoverView.swift
+//  CYOATemplate
+//
+//  Created by Griffin Muir on 2024-06-12.
+//
+
+import SwiftUI
+
+struct testCoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testCoverView()
+}
